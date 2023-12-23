3622d429222927890a0405b01839a94e51011274
abkalia <abhishekkalia66@gmail.com>
2023-12-23 18:47:31 +0530

    Doubly linked list started

--- Java/DSA/src/main/java/org/example/datastructures/linkedlist/Main.java
+++ Java/DSA/src/main/java/org/example/datastructures/linkedlist/Main.java
@@ -1,16 +1,17 @@
 package org.example.datastructures.linkedlist;
 
+import org.example.datastructures.doublylinkedlist.DoubleLinkedList;
+
 public class Main {
     public static void main(String[] args){
         LinkedList myLinkedList = new LinkedList(1);
         myLinkedList.append(2);
         myLinkedList.append(3);
-        myLinkedList.append(4);
-        myLinkedList.append(5);
-        myLinkedList.getLength();
-        myLinkedList.printList();
-        myLinkedList.reverseBetween(0,4);
-        myLinkedList.getLength();
-        myLinkedList.printList();
+//        myLinkedList.getLength();
+//        myLinkedList.printList();
+        DoubleLinkedList myDoublyLL = new DoubleLinkedList(1);
+        myDoublyLL.getHead();
+        myDoublyLL.getTail();
+        myDoublyLL.printList();
     }
 }
