d84613b04c45ab69f006d9fcf8e5aa2b3a460b0d
abkalia <abhishekkalia66@gmail.com>
2023-12-23 14:35:25 +0530

    LeetCode - LL Partition List (Good question)

--- Java/DSA/src/main/java/org/example/datastructures/linkedlist/Main.java
+++ Java/DSA/src/main/java/org/example/datastructures/linkedlist/Main.java
@@ -3,13 +3,15 @@ package org.example.datastructures.linkedlist;
 public class Main {
     public static void main(String[] args){
         LinkedList myLinkedList = new LinkedList(1);
-        myLinkedList.append(3);
-        myLinkedList.append(6);
         myLinkedList.append(8);
         myLinkedList.append(9);
         myLinkedList.append(10);
+        myLinkedList.append(3);
+        myLinkedList.append(6);
         myLinkedList.getLength();
         myLinkedList.printList();
-        System.out.println("\nkTh value is : " + myLinkedList.findKthFromEnd(7).value);
+        myLinkedList.partitionList(5);
+        myLinkedList.getLength();
+        myLinkedList.printList();
     }
 }
