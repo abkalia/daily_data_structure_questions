189f9992c6854ba0b5ac12d72d07f3a34033817f
abkalia <abhishekkalia66@gmail.com>
2024-03-08 11:21:17 +0530

    added stan questions

--- Java/DSA/src/main/java/org/example/datastructures/linkedlist/Main.java
+++ Java/DSA/src/main/java/org/example/datastructures/linkedlist/Main.java
@@ -1,17 +1,42 @@
 package org.example.datastructures.linkedlist;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        LinkedList myList = new LinkedList(1);
-        myList.append(2);
-        myList.append(3);
-        myList.append(4);
-        myList.append(5);
-        myList.printList();
+
+        Employee emp1 = new Employee("Abhishek","Calia",3);
+        Employee emp2 = new Employee("Abhishek","Ralia",7);
+        Employee emp3 = new Employee("rajat","Aingh",1);
+        List<Employee> myList = new ArrayList<>();
+        myList.add(emp1);
+        myList.add(emp2);
+        myList.add(emp3);
+        Comparator<Employee> com = (o1, o2) -> {
+            if(o1.lastName.compareTo(o2.lastName) > 0){
+                return 1;
+            } else {
+                return -1;
+            }
+        };
+        for(Employee em: myList){
+            System.out.println(em.lastName);
+        }
+        myList.sort(com);
+        System.out.println("\n sorted");
+        for(Employee em: myList){
+            System.out.println(em.lastName);
+        }
+    }
+}
+class Employee{
+    int id;
+    String firstName;
+    String lastName;
+
+    public Employee(String name, String last,int id){
+    this.firstName = name;
+    this.lastName = last;
+    this.id = id;
     }
 }
--- Java/DSA/src/main/java/org/example/datastructures/stan/Main.java
+++ Java/DSA/src/main/java/org/example/datastructures/stan/Main.java
@@ -1,15 +1,42 @@
 package org.example.datastructures.stan;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args){
-        String myString = "abcab";
-        int middle = myString.length()/2;
-        System.out.println("Middle value " + middle);
-        System.out.println(myString.substring(0,middle));
-        if(myString.length() % 2 == 0){
-            System.out.println(myString.substring(middle));
-        } else {
-            System.out.println(myString.substring(middle+1));
+
+        Employee emp1 = new Employee("Abhishek","Calia",3);
+        Employee emp2 = new Employee("Abhishek","Ralia",7);
+        Employee emp3 = new Employee("rajat","Aingh",1);
+        List<Employee> myList = new ArrayList<>();
+        myList.add(emp1);
+        myList.add(emp2);
+        myList.add(emp3);
+        Comparator<Employee> com = (o1, o2) -> {
+            if(o1.lastName.compareTo(o2.lastName) > 0){
+                return 1;
+            } else {
+                return -1;
+            }
+        };
+        for(Employee em: myList){
+            System.out.println(em.lastName);
         }
+        myList.sort(com);
+        System.out.println("\n sorted");
+        for(Employee em: myList){
+            System.out.println(em.lastName);
+        }
+    }
+}
+class Employee{
+    int id;
+    String firstName;
+    String lastName;
+
+    public Employee(String name, String last,int id){
+        this.firstName = name;
+        this.lastName = last;
+        this.id = id;
     }
 }
